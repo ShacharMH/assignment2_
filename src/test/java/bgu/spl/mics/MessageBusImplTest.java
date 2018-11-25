8759d27cc43c9a0c55384c97633463fc06a2884f
amirzim <amirzim@post.bgu.ac.il>
2018-11-25 16:28:58 +0200

    additions on MessageBusImplementation

--- src/test/java/bgu/spl/mics/MessageBusImplTest.java
+++ src/test/java/bgu/spl/mics/MessageBusImplTest.java
@@ -9,13 +9,56 @@ import static org.junit.Assert.*;
 
 ///AMIR
 
-public class MessageBusImplTest extends Future {
+public class MessageBusImplTest implements MessageBus {
 
     @Before
     public void setUp() throws Exception {
+        this.
     }
 
     @After
     public void tearDown() throws Exception {
+    }
+
+    @Test
+    //PRE: recieve some sort of an event, and a micro-service interested in handling it
+    //POST:
+    public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+
+    }
+
+    @Override
+    public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+
+    }
+
+    @Override
+    public <T> void complete(Event<T> e, T result) {
+
+    }
+
+    @Override
+    public void sendBroadcast(Broadcast b) {
+
+    }
+
+    @Override
+    public <T> Future<T> sendEvent(Event<T> e) {
+        return null;
+    }
+
+    @Override
+    public void register(MicroService m) {
+
+    }
+
+    @Override
+    public void unregister(MicroService m) {
+
+    }
+
+    @Override
+    public Message awaitMessage(MicroService m) throws InterruptedException {
+        return null;
     }
 }
