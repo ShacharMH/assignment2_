e39f790904b69b89a72b4b7b88d23404cb5c2f9b
shacharMH <shacharmh@gmail.com>
2018-12-09 14:01:30 +0200

    1. better implementation of resource service

--- src/main/java/bgu/spl/mics/application/services/ResourceService.java
+++ src/main/java/bgu/spl/mics/application/services/ResourceService.java
@@ -7,6 +7,9 @@ import bgu.spl.mics.application.messages.DeliveryEvent;
 import bgu.spl.mics.application.passiveObjects.DeliveryVehicle;
 import bgu.spl.mics.application.passiveObjects.ResourcesHolder;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 /**
  * ResourceService is in charge of the store resources - the delivery vehicles.
  * Holds a reference to the {@link ResourceHolder} singleton of the store.
@@ -19,22 +22,105 @@ import bgu.spl.mics.application.passiveObjects.ResourcesHolder;
 
 /* IS RESOURCE-HOLDER THREAD SAFE?
 deals with getting the car to deliver the book.
-1. should acquire the car
-2. send a DeliveryEvent, and wait until it is resolved
-3. return the car to the ResourcesHolder.
-4. complete the action by sending isDone = true;
  */
 public class ResourceService extends MicroService{
 
 	private ResourcesHolder resourcesHolder;
+	private BlockingQueue<WaitingDelivery> onGoingDeliveryQueue;
+	//private BlockingQueue<Future<Boolean>> onGoingDeliveryEvent;
 
 	public ResourceService() {
 		super("ResourceService");
 		resourcesHolder = ResourcesHolder.getInstance();
+		onGoingDeliveryQueue = new LinkedBlockingQueue<>();
 	}
+
 
 	@Override
 	protected void initialize() {
+
+		subscribeEvent(AcquireVehicleEvent.class, AcquireVehicleEventCallBack -> {
+			/*
+			1. try to acquire a vehicle
+				a. successful?
+					i. send a delivery event
+					ii. push delivery event to onGoingDeliveryQueue
+					iii. perform the function releaseVehicles once. - if onGoing... .size() == 0 then return true.
+				b. unsuccessful?
+					i. perform releaseVehicles until it returns "true" (e.g. released one or more vehicles.
+					ii. return to (1).
+			 */
+
+			Future<DeliveryVehicle> deliveryVehicleFuture = resourcesHolder.acquireVehicle();
+			boolean successful = false;
+			while (!successful) {
+				if (deliveryVehicleFuture.isDone()) { // a. successful
+					DeliveryVehicle myDeliveryVehicle = deliveryVehicleFuture.get();
+					DeliveryEvent deliveryEvent = new DeliveryEvent(myDeliveryVehicle,
+							AcquireVehicleEventCallBack.getAddress(),
+							AcquireVehicleEventCallBack.getDistance());
+					Future<Boolean> deliveryEventFuture = sendEvent(deliveryEvent); // i. send a delivery event
+					onGoingDeliveryQueue.add(new WaitingDelivery(deliveryEventFuture, myDeliveryVehicle, AcquireVehicleEventCallBack)); // ii. push delivery event to onGoingDeliveryQueue
+					releaseVehicles(); // perform the function releaseVehicles once
+					successful = true;
+				} else { // b. unsuccessful
+					while (!releaseVehicles()) ;
+					/* explanation:
+					if releaseVehicles == true, then there are free vehicles, and we return to 1 (e.g., the beginning of the while loop)
+					if releaseVehicles == false, then there are no free vehicles, and we'll call this function again.
+					 */
+				}
+			}
+		});
+	}
+
+	public static class WaitingDelivery {
+		private Future<Boolean> future;
+		private DeliveryVehicle deliveryVehicle;
+		private AcquireVehicleEvent acquireVehicleEvent;
+
+		public WaitingDelivery(Future<Boolean> future, DeliveryVehicle deliveryVehicle, AcquireVehicleEvent acquireVehicleEvent) {
+			this.future = future;
+			this.deliveryVehicle = deliveryVehicle;
+			this.acquireVehicleEvent = acquireVehicleEvent;
+		}
+
+		public Future<Boolean> getFuture() {
+			return future;
+		}
+
+		public DeliveryVehicle getDeliveryVehicle() {
+			return deliveryVehicle;
+		}
+
+		public AcquireVehicleEvent getAcquireVehicleEvent() {
+			return acquireVehicleEvent;
+		}
+	}
+
+	private boolean releaseVehicles() {
+		boolean isReleased = false;
+		for (WaitingDelivery waitingDelivery : onGoingDeliveryQueue) {
+			if (waitingDelivery.getFuture().isDone()) {
+				resourcesHolder.releaseVehicle(waitingDelivery.getDeliveryVehicle());
+				complete(waitingDelivery.getAcquireVehicleEvent(), true);
+				isReleased = true;
+			}
+		}
+		if (onGoingDeliveryQueue.size() == 0)
+			return true;
+		else
+			return isReleased;
+	}
+
+}
+
+/* NAIVE IMPLEMENTATION:
+
+		1. should acquire the car
+		2. send a DeliveryEvent, and wait until it is resolved
+		3. return the car to the ResourcesHolder.
+		4. complete the action by sending isDone = true;
 
 		subscribeEvent(AcquireVehicleEvent.class, AcquireVehicleEventCallback -> {
 			//1. acquiring a car
@@ -63,7 +149,6 @@ public class ResourceService extends MicroService{
 			// 4. complete the action by sending true
 			complete(AcquireVehicleEventCallback, true);
 		});
-		
-	}
 
-}
+*/
+
