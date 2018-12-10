a29732bbb3ea52fd8bee9cece97a02be1f1c598e
amirzim <amirzim@post.bgu.ac.il>
2018-12-10 13:13:36 +0200

    -messagebus: now all methods are either synchronized, or one line. Apart from await message that has a Blockingqueue.

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -23,8 +23,8 @@ public class MessageBusImpl implements MessageBus {
     private final ConcurrentHashMap<Event,Future> MapBetweenEventAndFutureObj;//addition, for finding the event its corresponding future object in method "complete"
     private final ConcurrentLinkedQueue<Class <? extends Event>> listOfTypesOfEvents;//every type of event, for "unregister" method
     private final ConcurrentLinkedQueue<Class<? extends Broadcast>> listOfTypesOfBroadcasts;//every type of broadcast, for "unregister" method
-    private ExampleEvent exampleEvent=new ExampleEvent("lalala");//just for testing
-    private Message message;//just for tests
+
+
 
     private static class BusHolder {
         private static MessageBusImpl instance = new MessageBusImpl();
@@ -57,16 +57,17 @@ public class MessageBusImpl implements MessageBus {
 
 
     //see if event already exists.  if so, add the ms to the event's queue. else, push the new couple (event, ms), while creating the ms's queue for the event.
-	 public synchronized   <T> void subscribeEvent (Class <? extends Event<T>> type, MicroService m) {
+	 public    <T> void subscribeEvent (Class <? extends Event<T>> type, MicroService m) {
+         synchronized(hashEventToMicroServiceQueue) {
              if (hashEventToMicroServiceQueue.containsKey(type)) {//if event already exists
                  hashEventToMicroServiceQueue.get(type).add(m);//get the queue of microservices for this type of event, add MS
-             }
-             else {
-                  ConcurrentLinkedQueue<MicroService> EventsMicroServiceList = new ConcurrentLinkedQueue<>();//create queue of microservices for this event
+             } else {
+                 ConcurrentLinkedQueue<MicroService> EventsMicroServiceList = new ConcurrentLinkedQueue<>();//create queue of microservices for this event
                  EventsMicroServiceList.add(m);//add micro-service to the queue,now this microservice(thread) can handle this event too
                  hashEventToMicroServiceQueue.put(type, EventsMicroServiceList);//add this new pair to the list of <events,microservices that handle these events>
                  listOfTypesOfEvents.add(type);
              }
+         }
 
      }
 
@@ -75,16 +76,16 @@ public class MessageBusImpl implements MessageBus {
 
 	//The same as above(subscribeEvent), for broadcasts
 	public  void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {//problem with compilation
+         synchronized (hashBroadcastToMicroServicesQueue) {
              if (hashBroadcastToMicroServicesQueue.containsKey(type)) {
-               hashBroadcastToMicroServicesQueue.get(type).add(m);
-             }
-             else {
+                 hashBroadcastToMicroServicesQueue.get(type).add(m);
+             } else {
                  ConcurrentLinkedQueue<MicroService> BroadcastsMicroserviceList = new ConcurrentLinkedQueue<>();
                  BroadcastsMicroserviceList.add(m);
                  hashBroadcastToMicroServicesQueue.put(type, BroadcastsMicroserviceList);
                  listOfTypesOfBroadcasts.add(type);
              }
-
+         }
     }
 
 
@@ -97,29 +98,32 @@ public class MessageBusImpl implements MessageBus {
 
 
 	public void sendBroadcast(Broadcast b) {//no need to synchronize,each broadcast type has its own queue of micro-services
-		for (MicroService item:hashBroadcastToMicroServicesQueue.get(b.getClass())) {//for every microservice that has subscribed to get this broadcast
-            hashMicroServiceToMessagesQueue.get(item).add(b);//get messagequeue of microservice, add message to it
+	     synchronized (hashBroadcastToMicroServicesQueue) {
+             for (MicroService item : hashBroadcastToMicroServicesQueue.get(b.getClass())) {//for every microservice that has subscribed to get this broadcast
+                 hashMicroServiceToMessagesQueue.get(item).add(b);//get messagequeue of microservice, add message to it
 
-        }
+             }
+         }
 	}
 
 	//Insert event to messagequeue of micro-service(round robin), return future object to micro-service that sent this event
     //need to synchronize this fellow
-	public synchronized  <T> Future<T>  sendEvent(Event<T> e) {
-	     if (hashEventToMicroServiceQueue.containsKey(e.getClass())) {
-             ConcurrentLinkedQueue<MicroService> QueueOfEvent = hashEventToMicroServiceQueue.get(e.getClass());//get the queue assigned to this type of event
-             MicroService handleEvent = QueueOfEvent.remove();//holds the micro service that will get the event and handle it.
-             System.out.println(" the name of the micro service that will handle is "+ handleEvent.getName());//test I added
-            LinkedBlockingQueue<Message> QueueOfMicroservice = hashMicroServiceToMessagesQueue.get(handleEvent);//get the message queue of the corresponding micro-service
-             if(QueueOfMicroservice==null) System.out.println("the queue is null");
-             QueueOfMicroservice.add(e);//"add" and not "put", this is a non-blocking method,
-             QueueOfEvent.add(handleEvent);//MicroService is returned to the tail of the queue of the event, round robin invariant is maintained.
-             Future<T> result = new Future<>();//create future object that will hold the answer to this event
-             MapBetweenEventAndFutureObj.put(e,result);//put event and future object in map
-             return result;
+	public   <T> Future<T>  sendEvent(Event<T> e) {
+	     synchronized (hashEventToMicroServiceQueue) {
+             if (hashEventToMicroServiceQueue.containsKey(e.getClass())) {
+                 ConcurrentLinkedQueue<MicroService> QueueOfEvent = hashEventToMicroServiceQueue.get(e.getClass());//get the queue assigned to this type of event
+                 MicroService handleEvent = QueueOfEvent.remove();//holds the micro service that will get the event and handle it.
+                 System.out.println(" the name of the micro service that will handle is " + handleEvent.getName());//test I added
+                 LinkedBlockingQueue<Message> QueueOfMicroservice = hashMicroServiceToMessagesQueue.get(handleEvent);//get the message queue of the corresponding micro-service
+                 if (QueueOfMicroservice == null) System.out.println("the queue is null");
+                 QueueOfMicroservice.add(e);//"add" and not "put", this is a non-blocking method,
+                 QueueOfEvent.add(handleEvent);//MicroService is returned to the tail of the queue of the event, round robin invariant is maintained.
+                 Future<T> result = new Future<>();//create future object that will hold the answer to this event
+                 MapBetweenEventAndFutureObj.put(e, result);//put event and future object in map
+                 return result;
+             } else
+                 return null;
          }
-	     else
-	         return null;
 	}
 
 
@@ -129,7 +133,7 @@ public class MessageBusImpl implements MessageBus {
     }
 
 
-	public void unregister(MicroService m) {
+	public synchronized void unregister(MicroService m) {
         hashMicroServiceToMessagesQueue.remove(m, hashMicroServiceToMessagesQueue.get(m));//remove MS and its messageQueue
         for (Class<? extends Event> type : listOfTypesOfEvents)//for every type of event, erase the micro service from its handling list
         {
@@ -143,7 +147,7 @@ public class MessageBusImpl implements MessageBus {
 
 //need to understand the difference between waiting for a message and being in the middle of handling one
         public Message awaitMessage (MicroService m) throws InterruptedException {//need to use interupted mechanism
-            if (!hashMicroServiceToMessagesQueue.containsKey(m)) {
+            if (!hashMicroServiceToMessagesQueue.containsKey(m)) {//if we are trying to get a meesage of a MS that isn't registered
                 throw new IllegalStateException();
             }
             try {
@@ -152,12 +156,6 @@ public class MessageBusImpl implements MessageBus {
                 System.out.println("await message thread interupted");
                 throw new InterruptedException();
             }
-            //finally {
-            //  Message handleNow = hashMicroServiceToMessagesQueue.get(m).take();//get the last Message from the message Queue
-            //    return handleNow;
-            //  }
-
-            //while(hashMicroServiceToMessagesQueue.isEmpty())
 
         }
 
